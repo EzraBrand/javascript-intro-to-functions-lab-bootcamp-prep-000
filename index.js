f988bd5faa7e023f2716d5de903e4a56ac16cbd2
Ezra Brand <ezrabrand@gmail.com>
2019-04-01 18:32:36 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,17 @@
+function shout(string) {
+    return string.toUpperCase()
+}
+
+shout('hello')
+
+function whisper(string) {
+  return string.toLowerCase()
+}
+
+function logShout(string) {
+  console.log(string.toUpperCase)
+}
+
+function logWhisper(string) {
+  console.log(string.toLowerCase)
+}
